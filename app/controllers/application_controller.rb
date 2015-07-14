60e788e9cf3e11b1bd0074bc2551ee0ea77fbef4
Mosheman <moshehefra.landaeta@usach.cl>
2015-07-13 23:59:35 -0300

    Add partials, Sign Up & Sign In Styles & error displays

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :store_current_location, :unless => :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(:superuser) ? stored_location_for(:superuser) : dashboard_index_path
+  end
+
+  def store_current_location
+    stored_location_for(:user)
+  end
+
 end
