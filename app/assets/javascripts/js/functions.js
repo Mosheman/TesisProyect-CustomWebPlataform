dda4cdb3b59a0c761ddfcb25552ec08ec5decc91
mosheman <moshehefra.landaeta@usach.cl>
2016-07-11 00:30:53 -0300

    Fix Markercluster and UISlider

--- app/assets/javascripts/js/functions.js
+++ app/assets/javascripts/js/functions.js
@@ -10,5 +10,9 @@ $('#myTabs a').click(function (e) {
 function slideUISlider(handle_id, value, text) {
 	console.log("Values: "+handle_id+"," +value+ ","+text);
 }
+
+function changedRadiusUISlider(e, ui) {
+	
+}
 // $("#searchtype-popover").popover({ trigger: "hover" });
 // $('#searchtype-popover').popover();
